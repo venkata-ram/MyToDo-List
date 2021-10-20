22d431a40b7ec45e88f8088f43d9c64793159240
venkat <v.kannan@infocerv.com>
2021-10-20 12:22:10 +0530

    show alert dialog for delete all

--- app/src/main/java/com/buildappswithvenkat/mytodoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/buildappswithvenkat/mytodoapp/fragments/list/ListFragment.kt
@@ -1,7 +1,9 @@
 package com.buildappswithvenkat.mytodoapp.fragments.list
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -47,6 +49,29 @@ class ListFragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.list_fragment_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.menu_delete_all){
+            confirmRemoval()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun confirmRemoval() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes"){ _, _ ->
+            mToDoViewModel.deleteAll()
+            Toast.makeText(
+                requireContext(),
+                "Successfully deleted...",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        builder.setNegativeButton("No"){ _, _-> }
+        builder.setTitle("Delete All?")
+        builder.setMessage("Are you sure you want to remove all items?")
+        builder.create().show()
     }
 
 }
